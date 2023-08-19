692602bf254ad979ea50977324604721af65b956
Arinomurakumo <14089757+arinomurakumo@users.noreply.github.com>
2023-08-20 00:41:16 +0900

    fix: statemant 関数の分割 - 関数化

--- src/episode-1/statement.js
+++ src/episode-1/statement.js
@@ -10,7 +10,7 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0
+    let thisAmount = amountFor(perf, play)
 
     switch (play.type) {
       case 'tragedy':
@@ -44,4 +44,26 @@ export function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
+
+  function amountFor(perf, play) {
+    let thisAmount = 0
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30)
+        }
+        break
+      case 'comedy':
+        thisAmount = 30000
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20)
+        }
+        thisAmount += 300 * perf.audience
+        break
+      default:
+        throw new Error(`unknown type: ${play.type}`)
+    }
+    return thisAmount
+  }
 }
