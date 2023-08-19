db412f9cb21dab8cd8ed2eeda4ea9d94ef9221b7
Arinomurakumo <14089757+arinomurakumo@users.noreply.github.com>
2023-08-20 00:41:54 +0900

    fix: statemant 関数の分割 - 不要な記述を削除

--- src/episode-1/statement.js
+++ src/episode-1/statement.js
@@ -12,24 +12,6 @@ export function statement(invoice, plays) {
     const play = plays[perf.playID]
     let thisAmount = amountFor(perf, play)
 
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-      case 'comedy':
-        thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
-        }
-        thisAmount += 300 * perf.audience
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
-
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0)
     // 喜劇の時は 10 人につき、さらにポイントを加算
