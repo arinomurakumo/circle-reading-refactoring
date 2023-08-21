b507c076a23b2d347e13b11fc95237af3c55a07e
Arinomurakumo <14089757+arinomurakumo@users.noreply.github.com>
2023-08-22 00:11:49 +0900

    fix: フェーズを分離 - 合計値を  enrichPerformance へ移動

--- src/episode-1/statement.js
+++ src/episode-1/statement.js
@@ -2,6 +2,8 @@ export function statement(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData)
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
   return renderPlainText(statementData, plays)
 
   function enrichPerformance(aPerformance) {
@@ -45,6 +47,22 @@ export function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5)
     return result
   }
+
+  function totalAmount(data) {
+    let result = 0
+    for (let perf of data.performances) {
+      result += perf.amount
+    }
+    return result
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0
+    for (let perf of data.performances) {
+      result += perf.volumeCredits
+    }
+    return result
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -55,8 +73,8 @@ function renderPlainText(data, plays) {
     } seats)\n`
   }
 
-  result += `Amount owed is ${usd(totalAmount())}\n`
-  result += `You earned ${totalVolumeCredits()} credits\n`
+  result += `Amount owed is ${usd(data.totalAmount)}\n`
+  result += `You earned ${data.totalVolumeCredits} credits\n`
   return result
 
   function usd(aNumber) {
@@ -65,21 +83,5 @@ function renderPlainText(data, plays) {
       currency: 'USD',
       minimumFractionDigits: 2
     }).format(aNumber / 100)
-  }
-
-  function totalVolumeCredits() {
-    let result = 0
-    for (let perf of data.performances) {
-      result += perf.volumeCredits
-    }
-    return result
-  }
-
-  function totalAmount() {
-    let result = 0
-    for (let perf of data.performances) {
-      result += perf.amount
-    }
-    return result
   }
 }
