b3b61184323b38b105827ec54d55296e54ce8a27
Arinomurakumo <14089757+arinomurakumo@users.noreply.github.com>
2023-08-22 01:33:03 +0900

    型による計算処理の再編成 - ボリューム特典ポイントの関数移動と関数インライン化

--- src/episode-1/createStatementData.js
+++ src/episode-1/createStatementData.js
@@ -14,7 +14,7 @@ export function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play
     result.amount = calculator.amount
-    result.volumeCredits = volumeCreditsFor(result)
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
@@ -27,11 +27,8 @@ export function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5)
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits
   }
 
   function totalAmount(data) {
@@ -68,6 +65,14 @@ class PerformanceCalculator {
       default:
         throw new Error(`unknown type: ${this.play.type}`)
     }
+    return result
+  }
+
+  get volumeCredits() {
+    let result = 0
+    result += Math.max(this.performance.audience - 30, 0)
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performance.audience / 5)
     return result
   }
 }
