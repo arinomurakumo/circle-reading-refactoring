410b6e0aa64402b7cfd1f828816372c7987710e2
Arinomurakumo <14089757+arinomurakumo@users.noreply.github.com>
2023-08-22 00:00:58 +0900

    fix: フェーズを分離 - 公演を enrichPerformance へ移動

--- src/episode-1/statement.js
+++ src/episode-1/statement.js
@@ -6,14 +6,19 @@ export function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
+    result.play = playFor(result)
     return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
   }
 }
 
 function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`
   for (let perf of data.performances) {
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`
   }
@@ -24,7 +29,7 @@ function renderPlainText(data, plays) {
 
   function amountFor(aPerformance) {
     let result = 0
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy':
         result = 40000
         if (aPerformance.audience > 30) {
@@ -39,19 +44,15 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+        throw new Error(`unknown type: ${aPerformance.play.type}`)
     }
     return result
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
   }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === playFor(aPerformance).type)
+    if ('comedy' === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5)
     return result
   }
