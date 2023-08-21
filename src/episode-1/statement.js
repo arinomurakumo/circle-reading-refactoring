f8dd8b387de15e235c2de66f710926092393144f
Arinomurakumo <14089757+arinomurakumo@users.noreply.github.com>
2023-08-22 00:03:56 +0900

    fix: フェーズを分離 - 金額を enrichPerformance へ移動

--- src/episode-1/statement.js
+++ src/episode-1/statement.js
@@ -7,25 +7,13 @@ export function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
+    result.amount = amountFor(result)
     return result
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
   }
-}
-
-function renderPlainText(data, plays) {
-  let result = `Statement for ${data.customer}\n`
-  for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    } seats)\n`
-  }
-
-  result += `Amount owed is ${usd(totalAmount())}\n`
-  result += `You earned ${totalVolumeCredits()} credits\n`
-  return result
 
   function amountFor(aPerformance) {
     let result = 0
@@ -48,6 +36,19 @@ function renderPlainText(data, plays) {
     }
     return result
   }
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`
+  result += `You earned ${totalVolumeCredits()} credits\n`
+  return result
 
   function volumeCreditsFor(aPerformance) {
     let result = 0
@@ -76,7 +77,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0
     for (let perf of data.performances) {
-      result += amountFor(perf)
+      result += perf.amount
     }
     return result
   }
