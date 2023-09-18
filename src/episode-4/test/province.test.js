06035ed0a1d2dca21194b35812939250867381ce
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-18 20:51:28 +0900

    Add good case

--- src/episode-4/test/province.test.js
+++ src/episode-4/test/province.test.js
@@ -3,14 +3,15 @@ const { Province } = require("../province");
 const { sampleProvinceData } = require("../sampleProvinceData");
 
 describe("province", function () {
-  const asia = new Province(sampleProvinceData()); // これはやってはいけない
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
 
   it("shortfall", function () {
-    // const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
-    // const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
