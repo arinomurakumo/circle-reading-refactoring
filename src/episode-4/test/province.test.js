945a797b758d53f61f6f561200861cc2256dad07
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-18 20:50:51 +0900

    Add bad case

--- src/episode-4/test/province.test.js
+++ src/episode-4/test/province.test.js
@@ -3,12 +3,14 @@ const { Province } = require("../province");
 const { sampleProvinceData } = require("../sampleProvinceData");
 
 describe("province", function () {
+  const asia = new Province(sampleProvinceData()); // これはやってはいけない
+
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
+    // const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
+    // const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
