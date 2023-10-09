76d772cda8f1c6a440cd11d48bb24d16f8447ad2
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:12:48 +0900

    3-ガード節による入れ子の条件記述の置き換え - Add サンプルコード-条件を逆転する

---
+++ src/episode-10/case3-2.js
@@ -0,0 +1,12 @@
+// 条件を逆転する
+function adjustedCapital(anInstrument) {
+  let result = 0;
+  if (anInstrument.capital > 0) {
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+      result =
+        (anInstrument.income / anInstrument.duration) *
+        anInstrument.adjustmentFactor;
+    }
+  }
+  return result;
+}
