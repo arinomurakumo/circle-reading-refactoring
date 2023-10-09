b4166df189f3dc084eedfc293b495d82beabd8d1
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 14:47:50 +0900

    1-条件記述の分解 - Fix then節を関数に抽出

--- src/episode-10/case1.js
+++ src/episode-10/case1.js
@@ -1,7 +1,12 @@
-if (summer()) charge = quantity * plan.summerRate;
+// こちらにあった then節 を
+if (summer()) charge = summerCharge();
 else charge = quantity * plan.summerRate + plan.regularServiceCharge;
 
-// 条件記述 を 関数 に抽出します。
 function summer() {
   return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+
+// 新しい関数 に 抽出します。
+function summerCharge() {
+  return quantity * plan.summerRate;
 }
