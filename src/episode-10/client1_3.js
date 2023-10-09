d09750352185800b9dbb359c8ab32ac30334f537
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 21:33:54 +0900

    5-特殊ケースの導入 - Add 「変換を利用する」のコード追加

---
+++ src/episode-10/client1_3.js
@@ -0,0 +1,9 @@
+// client1
+
+const site = acquireSiteData();
+const aCustomer = site.customer;
+// ... 大量のコードが入る ...
+
+let customerName;
+if (aCustomer === "unknown") customer.Name = "occupant";
+else customerName = aCustomer.name;
---
+++ src/episode-10/client3_3.js
@@ -0,0 +1,6 @@
+// client3
+
+const weeksDelinquent =
+  aCustomer === "unknown"
+    ? 0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
