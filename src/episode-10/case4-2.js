c4f132b174c92d682fecd6169a6cd036248d8f5d
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:30:51 +0900

    4-バリエーションに対してポリモーフィズムを適用する - Fix 船長が過去に中国への航海経験がある場合のコメント追加

--- src/episode-10/case4-2.js
+++ src/episode-10/case4-2.js
@@ -1,9 +1,3 @@
-// バリエーションに対してポリモーフィズムを適用する
-
-// 格付け機関 が 船の航行に対する投資格付け を計算するために使用するコード
-// 格付け機関 は 潜在的なリスク と 利益に影響するさまざまな要素 を考慮して、A または B のいずれかを付与する。
-// リスク評価では、航海の性質だけでなく、船長の過去の航海実績 も考慮する。
-
 // 計算ポイントによって A / B を付与
 function rating(voyage, history) {
   const vpf = voyageProfitFactor(voyage, history);
@@ -27,6 +21,7 @@ function captainHistoryRisk(voyage, history) {
   let result = 1;
   if (voyage.length < 5) result += 4;
   result += history.filter((v) => v.profit < 0).length;
+  // 船長が過去に中国への航海経験がある場合
   if (voyage.zone === "china" && hasChina(history)) result -= 2;
   return Math.max(result, 0);
 }
@@ -41,6 +36,7 @@ function voyageProfitFactor(voyage, history) {
   let result = 2;
   if (voyage.zone === "china") result += 1;
   if (voyage.zone === "east-indies") result += 1;
+  // 船長が過去に中国への航海経験がある場合
   if (voyage.zone === "china" && hasChina(history)) {
     result += 3;
     if (history.length > 10) result += 1;
