708564b60e999cf95171e2a2a003153967676afa
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:13:46 +0900

    3-ガード節による入れ子の条件記述の置き換え - Fix ガード節に入れる条件分岐を逆にする

--- src/episode-10/case3-2.js
+++ src/episode-10/case3-2.js
@@ -1,12 +1,10 @@
-// 条件を逆転する
 function adjustedCapital(anInstrument) {
   let result = 0;
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result =
-        (anInstrument.income / anInstrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (anInstrument.capital <= 0) return result;
+  if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+    result =
+      (anInstrument.income / anInstrument.duration) *
+      anInstrument.adjustmentFactor;
   }
   return result;
 }
