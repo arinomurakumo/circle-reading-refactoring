efb8e7f61413d7afe7a82df942d4c0dab28338c6
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 14:41:29 +0900

    1-条件記述の分解 - Add 冬と夏で適用レートが異なる料金計算

---
+++ src/episode-10/case1.js
@@ -0,0 +1,3 @@
+if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+  charge = quantity * plan.summerRate;
+else charge = quantity * plan.summerRate + plan.regularServiceCharge;
