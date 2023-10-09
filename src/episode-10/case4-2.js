8953fb22bdb6c725e54499d9d3a53ad48a4ff141
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:35:34 +0900

    4-バリエーションに対してポリモーフィズムを適用する - Fix サブクラスの整備

--- src/episode-10/case4-2.js
+++ src/episode-10/case4-2.js
@@ -45,15 +45,8 @@ class Rating {
 
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    if (this.voyage.zone === "china" && this.hasChinaHistory) {
-      result += 3;
-      if (this.history.length > 10) result += 1;
-      if (this.history.length > 12) result += 1;
-      if (this.history.length > 18) result -= 1;
-    } else {
-      if (this.history.length > 8) result += 1;
-      if (this.history.length > 14) result -= 1;
-    }
+    if (this.history.length > 8) result += 1;
+    if (this.history.length > 14) result -= 1;
     return result;
   }
 
@@ -67,6 +60,15 @@ class ExperiencedChinaRating extends Rating {
   get captainHistoryRisk() {
     const result = super.captainHistoryRisk - 2;
     return Math.max(result, 0);
+  }
+
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    result += 3;
+    if (this.history.length > 10) result += 1;
+    if (this.history.length > 12) result += 1;
+    if (this.history.length > 18) result -= 1;
+    return result;
   }
 }
 
