afd1ba1fc892814e12d067084b210ad917514bca
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:09:37 +0900

    3-ガード節による入れ子の条件記述の置き換え - Fix 一番上の条件判定でガード節を使用

--- src/episode-10/case3-1.js
+++ src/episode-10/case3-1.js
@@ -1,25 +1,18 @@
-// 従業員への支払額を計算するコード
-
-// ロジックのテキストは、Lorem ipsum（ロレム。イプサム）と呼ばれるダミーテキスト
-
-// 従業員が会社に在籍している場合のみ 支払う
-// isSeparated（離職者）と isRetired（退職者）を 除外する必要がある
+// 主目的である 計算 は、2つの条件が当てはまらない従業員 に対してのみ適用されます。
+// このような場合、ガード節 を使用すると コードの意図 を明快に表現できる。
 function payAmount(employee) {
   let result;
-  if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: "SEP" };
+  if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" };
+  if (employee.isRetired) {
+    result = { amount: 0, reasonCode: "RET" };
   } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 金額を計算するロジック
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      setImmediate.do.eiusmod =
-        tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      ut.enim.ad(ninim.veniam);
-      result = someFinalComputation;
-    }
+    // 金額を計算するロジック
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    setImmediate.do.eiusmod =
+      tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(ninim.veniam);
+    result = someFinalComputation;
   }
   return result;
 }
