ed8e80e7e2934f1e19bf615d4aeccc66e0429c3f
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:24:09 +0900

    4-ポリモーフィズムによる条件記述の置き換え - Fix airSpeedVelocityの条件メソッドの変更

--- src/episode-10/case4-1.js
+++ src/episode-10/case4-1.js
@@ -22,15 +22,24 @@ class EuropeanSwallow extends Bird {
   get plumage() {
     return "avarage";
   }
+  get airSpeedVelocity() {
+    return 35;
+  }
 }
 class AfricanSwallow extends Bird {
   get plumage() {
     return this.numberOfCoconuts > 2 ? "tired" : "avarage";
   }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
 }
 class NorwegianBlueParrot extends Bird {
   get plumage() {
-    return this.bird.voltage > 100 ? "scorched" : "beautiful";
+    return this.voltage > 100 ? "scorched" : "beautiful";
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
   }
 }
 
@@ -38,21 +47,10 @@ class Bird {
   constructor(birdObject) {
     Object.assign(this, birdObject);
   }
-
   get plumage() {
     return "unknown";
   }
-
   get airSpeedVelocity() {
-    switch (this.type) {
-      case "EuropeanSwallow":
-        return 35;
-      case "AfricanSwallow":
-        return 40 - 2 * bird.numberOfCoconuts;
-      case "NorwegianBlueParrot":
-        return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
