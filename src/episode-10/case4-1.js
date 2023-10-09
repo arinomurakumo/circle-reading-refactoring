e685d2c69dac2af513a8e6bd4c1c3d25d8ff5ac7
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:19:37 +0900

    4-ポリモーフィズムによる条件記述の置き換え - Fix 関数群のクラスへの集約

--- src/episode-10/case4-1.js
+++ src/episode-10/case4-1.js
@@ -1,33 +1,38 @@
-// 鳥の情報を判定する小さなプログラム
-function plumages(birds) {
-  return new Map(birds.map(b >= [b.name, plumage(b)]));
-}
-function speeds(birds) {
-  return new Map(birds.map(b >= [b.name, airSpeedVelocity(b)]));
-}
-
 function plumage(bird) {
-  switch (bird.type) {
-    case "EuropeanSwallow":
-      return "avarage";
-    case "AfircanSwallow":
-      return bird.numberOfCoconuts > 2 ? "tired" : "avarage";
-    case "NorwegianBlueParrot":
-      return bird.voltage > 100 ? "scorched" : "beautiful";
-    default:
-      return "unknown";
-  }
+  return new Bird(bird).plumage;
+}
+function speed(bird) {
+  return new Bird(bird).airSpeedVelocity;
 }
 
-function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case "EuropeanSwallow":
-      return 35;
-    case "AfircanSwallow":
-      return 40 - 2 * bird.numberOfCoconuts;
-    case "NorwegianBlueParrot":
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    switch (this.type) {
+      case "EuropeanSwallow":
+        return "avarage";
+      case "AfircanSwallow":
+        return bird.numberOfCoconuts > 2 ? "tired" : "avarage";
+      case "NorwegianBlueParrot":
+        return bird.voltage > 100 ? "scorched" : "beautiful";
+      default:
+        return "unknown";
+    }
+  }
+
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case "EuropeanSwallow":
+        return 35;
+      case "AfircanSwallow":
+        return 40 - 2 * bird.numberOfCoconuts;
+      case "NorwegianBlueParrot":
+        return bird.isNailed ? 0 : 10 + bird.voltage / 10;
+      default:
+        return null;
+    }
   }
 }
