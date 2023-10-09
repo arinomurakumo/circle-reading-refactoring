249f5dc1ebdd676dad812a0458f0cdd4abb308ae
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 21:57:13 +0900

    6-アサーションの導入 - Fix アサーションの追加

--- src/episode-10/case6.js
+++ src/episode-10/case6.js
@@ -1,10 +1,9 @@
-// アサーションの導入
-
-// 顧客に対して設定した割引率 が すべての購入品目に適用される 例
-
 class Customer {
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
-    else return aNumber - this.discountRate * aNumber;
+    else {
+      assert(this.discountRate >= 0);
+      return aNumber - this.discountRate * aNumber;
+    }
   }
 }
