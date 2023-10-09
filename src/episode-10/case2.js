a86c57773484d2f0a2456eecaefddfb0e6c3cd7c
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:04:49 +0900

    2-条件記述の統合 - Fix and演算子 && を使う

--- src/episode-10/case2.js
+++ src/episode-10/case2.js
@@ -12,5 +12,5 @@ function disabilityAmount(anEmployee) {
 
 // and演算子（&&）を使う
 // 入れ子 の if文 を使っている場合
-if (anEmployee.onVacation) if (anEmployee.seniority > 10) return 1;
+if (anEmployee.onVacation && anEmployee.seniority > 10) return 1;
 return 0.5;
