9cae4ba13f58a2da430477ca4b4cd3f2c73f459a
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 21:37:25 +0900

    5-特殊ケースの導入 - Fix ディープコピーを行う

--- src/episode-10/client1_3.js
+++ src/episode-10/client1_3.js
@@ -1,9 +1,14 @@
 // client1
 
-const site = acquireSiteData();
+const rawSite = acquireSiteData();
+const site = enrichSite(rawSite);
 const aCustomer = site.customer;
 // ... 大量のコードが入る ...
 
 let customerName;
 if (aCustomer === "unknown") customer.Name = "occupant";
 else customerName = aCustomer.name;
+
+function enrichSite(inputSite) {
+  return _.cloneDeep(inputSite);
+}
