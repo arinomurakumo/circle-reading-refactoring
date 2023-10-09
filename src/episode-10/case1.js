7f235df369f9a7133a9d03a35820471503696836
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 14:43:21 +0900

    1-条件記述の分解 - Fix 条件記述を関数に抽出

--- src/episode-10/case1.js
+++ src/episode-10/case1.js
@@ -1,3 +1,7 @@
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
-  charge = quantity * plan.summerRate;
+if (summer()) charge = quantity * plan.summerRate;
 else charge = quantity * plan.summerRate + plan.regularServiceCharge;
+
+// 条件記述 を 関数 に抽出します。
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
