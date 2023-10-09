ec7480c9cf7652b22e78921e44ff6c7b933dbb5c
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 15:15:46 +0900

    3-ガード節による入れ子の条件記述の置き換え - Fix 条件記述の統合をする

--- src/episode-10/case3-2.js
+++ src/episode-10/case3-2.js
@@ -1,7 +1,10 @@
 function adjustedCapital(anInstrument) {
   let result = 0;
-  if (anInstrument.capital <= 0) return result;
-  if (anInstrument.interestRate <= 0 && anInstrument.duration <= 0)
+  if (
+    anInstrument.capital <= 0 ||
+    anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+  )
     return result;
   result =
     (anInstrument.income / anInstrument.duration) *
