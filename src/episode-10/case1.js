17771f9077478233fc60b1f2ec0043007bbff7d0
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-09 14:51:28 +0900

    1-条件記述の分解 - Fix 三項演算子を使って条件式を再フォーマット

--- src/episode-10/case1.js
+++ src/episode-10/case1.js
@@ -1,6 +1,5 @@
-if (summer()) charge = summerCharge();
-// こちらにあった else節 を
-else charge = regularCharge();
+// 三項演算子を使って条件式を再フォーマット
+charge = summer() ? summerCharge() : regularCharge();
 
 function summer() {
   return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
